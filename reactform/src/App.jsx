8e5404c0f307fbed9b54ba2755a8f65f41f607d2
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2023-11-30 11:37:29 +0530

    completedtheassignment

---
+++ reactform/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import SignUpForm from './Component/ReactForm'
+
+function App() {
+  
+
+  return (
+    <>
+     <SignUpForm/>
+    </>
+  )
+}
+
+export default App
